0b55041fca139ac0ab2f093af2e52db4514c2678
jake <rothjakem@gmail.com>
2021-06-20 09:34:15 -0500

    figs/output 105,110,115,120

---
+++ cascade_comparison_script_ics_all.m
@@ -0,0 +1,4 @@
+run('cascade_comparison_script_ics_1_05.m');
+run('cascade_comparison_script_ics_1_10.m');
+run('cascade_comparison_script_ics_1_15.m');
+run('cascade_comparison_script_ics_1_20.m');
