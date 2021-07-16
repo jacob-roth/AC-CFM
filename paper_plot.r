62b1a71693e90537ae3bc79ae2c8db4bc565a624
jake <rothjakem@gmail.com>
2021-07-16 12:22:42 -0500

    add stats calc

--- paper_plot.r
+++ paper_plot.r
@@ -48,10 +48,13 @@ protections <- c("allprotection","ol_vls_50")
 limitscales <- c("1_05","1_10","1_15","1_20")
 plottypes <- c("lines","loadlost_all")#,"loadserved_all")
 
-paper_plot <- function(plottype,casebase,methods,methodlabs,protections,limitscales,frac_or_pu,number_or_proportion,alternate_formatting,cond_or_uncond){
+paper_plot <- function(plottype,casebase,methods,methodlabs,protections,limitscales,frac_or_pu,number_or_proportion,alternate_formatting,cond_or_uncond,plotting){
   fnames <- c()
   fids <- c()
   dataout <- list()
+  dataavg <- list()
+  datastd <- list()
+  datamedcond <- list()
   for (protection in protections) {
     for (limitscale in limitscales) {
       ##
@@ -104,6 +107,9 @@ paper_plot <- function(plottype,casebase,methods,methodlabs,protections,limitsca
           }
           xx <- x[mask];
           yy <- y[mask];
+          avg <- mean(ypre[mask],na.rm=TRUE)
+          std <- sd(ypre[mask],na.rm=TRUE)
+          medcond <- median(ypre[(ypre > 0)],na.rm=TRUE)
           if (number_or_proportion == "proportion"){
             xlabel <- "L: Number of failed lines"
             ylabel <- "Pr(Number of failed lines >= L)"
@@ -128,6 +134,9 @@ paper_plot <- function(plottype,casebase,methods,methodlabs,protections,limitsca
             }
             xx <- x[mask];
             yy <- y[mask];
+            avg <- mean(ypre[mask],na.rm=TRUE)
+            std <- sd(ypre[mask],na.rm=TRUE)
+            medcond <- median(ypre[(ypre > 0)],na.rm=TRUE)
             if (number_or_proportion == "proportion"){
               xlabel <- "x: Load shed (p.u.)"
               ylabel <- "Pr(Load shed >= x)"
@@ -147,6 +156,9 @@ paper_plot <- function(plottype,casebase,methods,methodlabs,protections,limitsca
             }
             xx <- x[mask];
             yy <- y[mask];
+            avg <- mean(ypre[mask],na.rm=TRUE)
+            std <- sd(ypre[mask],na.rm=TRUE)
+            medcond <- median(ypre[(ypre > 0)],na.rm=TRUE)
             if (number_or_proportion == "proportion"){
               xlabel <- "x: Load shed (fraction)"
               ylabel <- "Pr(Load shed >= x)"
@@ -174,6 +186,9 @@ paper_plot <- function(plottype,casebase,methods,methodlabs,protections,limitsca
             }
             xx <- x[mask];
             yy <- y[mask];
+            avg <- mean(ypre[mask],na.rm=TRUE)
+            std <- sd(ypre[mask],na.rm=TRUE)
+            medcond <- median(ypre[(ypre > 0)],na.rm=TRUE)
             if (number_or_proportion == "proportion"){
               xlabel <- "x: Load served (p.u.)"
               ylabel <- "Pr(Load served >= x)"
@@ -195,6 +210,9 @@ paper_plot <- function(plottype,casebase,methods,methodlabs,protections,limitsca
             }
             xx <- x[mask];
             yy <- y[mask];
+            avg <- mean(ypre[mask],na.rm=TRUE)
+            std <- sd(ypre[mask],na.rm=TRUE)
+            medcond <- median(ypre[(ypre > 0)],na.rm=TRUE)
             if (number_or_proportion == "proportion"){
               xlabel <- "x: Load served (fraction)"
               ylabel <- "Pr(Load served >= x)"
@@ -222,8 +240,12 @@ paper_plot <- function(plottype,casebase,methods,methodlabs,protections,limitsca
           dataxproportion[[k]] <- xx
           maxlength <- max(maxlength,max(length(yy)))
         }
+        dataavg[[paste(protection,":",limitscale,":",method,":avg",sep='')]] <- avg
+        datastd[[paste(protection,":",limitscale,":",method,":std",sep='')]] <- std
+        datastd[[paste(protection,":",limitscale,":",method,":medcond",sep='')]] <- medcond
       }
     
+      ## pad each series
       for (k in 1:length(methodlabs)) {
         dataynumber[[k]] <- c(dataynumber[[k]], rep(NA, maxlength-length(dataynumber[[k]])))
         datayproportion[[k]] <- c(datayproportion[[k]], rep(NA, maxlength-length(datayproportion[[k]])))
@@ -238,75 +260,78 @@ paper_plot <- function(plottype,casebase,methods,methodlabs,protections,limitsca
       dataxproportion <- as.data.frame(dataxproportion); names(dataxproportion) <- methodlabsx
       datanumber <- cbind(dataynumber,dataxnumber)
       dataproportion <- cbind(datayproportion,dataxproportion)
+
       ##
       ## make plot
       ##
-      if (number_or_proportion == "number") {
-        data <- datanumber
-      } else if (number_or_proportion == "proportion") {
-        data <- dataproportion
+      if (plotting == TRUE) {
+        if (number_or_proportion == "number") {
+          data <- datanumber
+        } else if (number_or_proportion == "proportion") {
+          data <- dataproportion
+        }
+        dataout[[paste(protection,":",limitscale,sep='')]] <- data
+        # if (alternate_formatting == TRUE){
+        #   pdf(fnameout)
+        # } else {
+        #   pdf(fnameout)
+        # }
+        sp <- ggplot(data=data) + 
+              geom_line(aes(x=acopf_x, y=acopf_y, color = "N-0")) +
+              geom_line(aes(x=scacopf_x, y=scacopf_y, color = "N-1")) +
+              geom_line(aes(x=exitrates_1e09_x, y=exitrates_1e09_y, color = "lambda = 1e-09")) +
+              geom_line(aes(x=exitrates_1e12_x, y=exitrates_1e12_y, color = "lambda = 1e-12")) +
+              geom_line(aes(x=exitrates_1e15_x, y=exitrates_1e15_y, color = "lambda = 1e-15")) +
+              xlab(xlabel) +
+              ylab(ylabel) +
+              theme(plot.title = element_text(size = 11, face = "bold")) +
+              scale_color_manual(values = c("N-0" = "red",
+                                            "N-1" = "brown4",
+                                            "lambda = 1e-09" = "blue",
+                                            "lambda = 1e-12" = "forestgreen",
+                                            "lambda = 1e-15" = "magenta"),
+                                limits = c("N-0", "N-1",
+                                            "lambda = 1e-09", 
+                                            "lambda = 1e-12", 
+                                            "lambda = 1e-15"),
+                                # labels = c("N-0", "N-1",              #labels = c("N-0", "N-1",
+                                #   expression("λ"^"lim"~"="~10^"-9"),  #expression("\U03BB"^"lim"~"="~10^"-9"), 
+                                #   expression("λ"^"lim"~"="~10^"-12"), #expression("\U03BB"^"lim"~"="~10^"-12"), 
+                                #   expression("λ"^"lim"~"="~10^"-15")) #expression("\U03BB"^"lim"~"="~10^"-15"))
+                                labels = c("N-0", "N-1",
+                                            unname(TeX(c(
+                                              "$\\lambda^{\\mathrm{lim}}\\,=\\,10^{-9}$",
+                                              "$\\lambda^{\\mathrm{lim}}\\,=\\,10^{-12}$",
+                                              "$\\lambda^{\\mathrm{lim}}\\,=\\,10^{-15}$"
+                                            ))))
+              ) + labs(color = "")
+              if (TRUE) {
+                # http://www.sthda.com/english/wiki/ggplot2-axis-scales-and-transformations
+                ### albert
+                sp <- sp + scale_y_log10(breaks = trans_breaks("log10", function(x) 10^x, n=2),
+                                        labels = trans_format("log10", math_format(10^.x)),
+                                        minor_breaks=log10_minor_break())
+                sp <- sp + scale_x_log10(breaks = trans_breaks("log10", function(x) 10^x, n=2),
+                                        labels = trans_format("log10", math_format(10^.x)),
+                                        minor_breaks=log10_minor_break())
+                if (alternate_formatting == TRUE) {
+                #  sp <- sp + theme_bw()# + annotation_logticks()
+                sp <- sp + theme_bw(base_size = 16) + annotation_logticks(base = 10, sides = "lb")
+                }
+              } else {
+                if (alternate_formatting == TRUE) {
+                sp <- sp + theme_bw(base_size = 16)
+                }
+              }
+              # print(sp)
+              # dev.off()
+              path = "/Users/jakeroth/git/AC-CFM"
+              # ggsave(fnameout, device='png', path=path, dpi=700, width=8.96, height=6.44, units="in")
+              ggsave(fnameout, device='pdf', path=path, width=8.96, height=6.44, units="in")
       }
-      dataout[[paste(protection,":",limitscale,sep='')]] <- data
-      # if (alternate_formatting == TRUE){
-      #   pdf(fnameout)
-      # } else {
-      #   pdf(fnameout)
-      # }
-      sp <- ggplot(data=data) + 
-            geom_line(aes(x=acopf_x, y=acopf_y, color = "N-0")) +
-            geom_line(aes(x=scacopf_x, y=scacopf_y, color = "N-1")) +
-            geom_line(aes(x=exitrates_1e09_x, y=exitrates_1e09_y, color = "lambda = 1e-09")) +
-            geom_line(aes(x=exitrates_1e12_x, y=exitrates_1e12_y, color = "lambda = 1e-12")) +
-            geom_line(aes(x=exitrates_1e15_x, y=exitrates_1e15_y, color = "lambda = 1e-15")) +
-            xlab(xlabel) +
-            ylab(ylabel) +
-            theme(plot.title = element_text(size = 11, face = "bold")) +
-            scale_color_manual(values = c("N-0" = "red",
-                                          "N-1" = "brown4",
-                                          "lambda = 1e-09" = "blue",
-                                          "lambda = 1e-12" = "forestgreen",
-                                          "lambda = 1e-15" = "magenta"),
-                              limits = c("N-0", "N-1",
-                                          "lambda = 1e-09", 
-                                          "lambda = 1e-12", 
-                                          "lambda = 1e-15"),
-                              # labels = c("N-0", "N-1",              #labels = c("N-0", "N-1",
-                              #   expression("λ"^"lim"~"="~10^"-9"),  #expression("\U03BB"^"lim"~"="~10^"-9"), 
-                              #   expression("λ"^"lim"~"="~10^"-12"), #expression("\U03BB"^"lim"~"="~10^"-12"), 
-                              #   expression("λ"^"lim"~"="~10^"-15")) #expression("\U03BB"^"lim"~"="~10^"-15"))
-                              labels = c("N-0", "N-1",
-                                          unname(TeX(c(
-                                            "$\\lambda^{\\mathrm{lim}}\\,=\\,10^{-9}$",
-                                            "$\\lambda^{\\mathrm{lim}}\\,=\\,10^{-12}$",
-                                            "$\\lambda^{\\mathrm{lim}}\\,=\\,10^{-15}$"
-                                          ))))
-            ) + labs(color = "")
-            if (TRUE) {
-              # http://www.sthda.com/english/wiki/ggplot2-axis-scales-and-transformations
-              ### albert
-              sp <- sp + scale_y_log10(breaks = trans_breaks("log10", function(x) 10^x, n=2),
-                                      labels = trans_format("log10", math_format(10^.x)),
-                                      minor_breaks=log10_minor_break())
-              sp <- sp + scale_x_log10(breaks = trans_breaks("log10", function(x) 10^x, n=2),
-                                      labels = trans_format("log10", math_format(10^.x)),
-                                      minor_breaks=log10_minor_break())
-              if (alternate_formatting == TRUE) {
-              #  sp <- sp + theme_bw()# + annotation_logticks()
-               sp <- sp + theme_bw(base_size = 16) + annotation_logticks(base = 10, sides = "lb")
-              }
-            } else {
-              if (alternate_formatting == TRUE) {
-               sp <- sp + theme_bw(base_size = 16)
-              }
-            }
-            # print(sp)
-            # dev.off()
-            path = "/Users/jakeroth/git/AC-CFM"
-            # ggsave(fnameout, device='png', path=path, dpi=700, width=8.96, height=6.44, units="in")
-            ggsave(fnameout, device='pdf', path=path, width=8.96, height=6.44, units="in")
     }
   }
-  return(dataout)
+  return(c(dataout,dataavg,datastd,datamedcond))
 }
 
 ##
@@ -316,6 +341,7 @@ cond_or_uncond <- "cond"
 frac_or_pu <- "pu"
 number_or_proportion <- "proportion"
 alternate_formatting <- TRUE
+plotting <- TRUE
 for (plottype in plottypes){
   paper_plot(plottype,casebase,methods,methodlabs,protections,limitscales,frac_or_pu,number_or_proportion, alternate_formatting, cond_or_uncond)
 }
@@ -324,6 +350,7 @@ cond_or_uncond <- "uncond"
 frac_or_pu <- "pu"
 number_or_proportion <- "proportion"
 alternate_formatting <- TRUE
+plotting <- TRUE
 for (plottype in plottypes){
   paper_plot(plottype,casebase,methods,methodlabs,protections,limitscales,frac_or_pu,number_or_proportion, alternate_formatting, cond_or_uncond)
 }
@@ -336,16 +363,18 @@ cond_or_uncond <- "cond"
 frac_or_pu <- "pu"
 number_or_proportion <- "number"
 alternate_formatting <- TRUE
+plotting <- TRUE
 for (plottype in plottypes){
-  paper_plot(plottype,casebase,methods,methodlabs,protections,limitscales,frac_or_pu,number_or_proportion, alternate_formatting, cond_or_uncond)
+  paper_plot(plottype,casebase,methods,methodlabs,protections,limitscales,frac_or_pu,number_or_proportion, alternate_formatting, cond_or_uncond,plotting)
 }
 
 cond_or_uncond <- "uncond"
 frac_or_pu <- "pu"
 number_or_proportion <- "number"
 alternate_formatting <- TRUE
+plotting <- TRUE
 for (plottype in plottypes){
-  paper_plot(plottype,casebase,methods,methodlabs,protections,limitscales,frac_or_pu,number_or_proportion, alternate_formatting, cond_or_uncond)
+  paper_plot(plottype,casebase,methods,methodlabs,protections,limitscales,frac_or_pu,number_or_proportion, alternate_formatting, cond_or_uncond,plotting)
 }
 
 
@@ -356,19 +385,67 @@ cond_or_uncond <- "uncond"
 frac_or_pu <- "pu"
 number_or_proportion <- "proportion"
 alternate_formatting <- TRUE
+plotting <- TRUE
 for (plottype in plottypes){
-  paper_plot(plottype,casebase,methods,methodlabs,protections,limitscales,frac_or_pu,number_or_proportion, alternate_formatting, cond_or_uncond)
+  paper_plot(plottype,casebase,methods,methodlabs,protections,limitscales,frac_or_pu,number_or_proportion, alternate_formatting, cond_or_uncond,plotting)
 }
 
 cond_or_uncond <- "uncond"
 frac_or_pu <- "pu"
 number_or_proportion <- "number"
 alternate_formatting <- TRUE
-for (plottype in plottypes){
-  D<-paper_plot(plottype,casebase,methods,methodlabs,protections,limitscales,frac_or_pu,number_or_proportion, alternate_formatting, cond_or_uncond)
-}
+plotting <- FALSE
+protections <- c("allprotection")
+Dlines<-paper_plot("lines",casebase,methods,methodlabs,protections,limitscales,frac_or_pu,number_or_proportion, alternate_formatting, cond_or_uncond,plotting)
+Dload<-paper_plot("loadlost_all",casebase,methods,methodlabs,protections,limitscales,frac_or_pu,number_or_proportion, alternate_formatting, cond_or_uncond,plotting)
 
+avglines <- c(Dlines$`allprotection:1_20:acopf:avg`,
+              Dlines$`allprotection:1_20:scacopf:avg`,
+              Dlines$`allprotection:1_20:fpacopf_09:avg`,
+              Dlines$`allprotection:1_20:fpacopf_12:avg`,
+              Dlines$`allprotection:1_20:fpacopf_15:avg`)
+avgload <-  c(Dload$`allprotection:1_20:acopf:avg`,
+              Dload$`allprotection:1_20:scacopf:avg`,
+              Dload$`allprotection:1_20:fpacopf_09:avg`,
+              Dload$`allprotection:1_20:fpacopf_12:avg`,
+              Dload$`allprotection:1_20:fpacopf_15:avg`)
+avglines <- c(Dlines$`allprotection:1_20:acopf:avg`,
+              Dlines$`allprotection:1_20:scacopf:avg`,
+              Dlines$`allprotection:1_20:fpacopf_09:avg`,
+              Dlines$`allprotection:1_20:fpacopf_12:avg`,
+              Dlines$`allprotection:1_20:fpacopf_15:avg`)
+stdlines <- c(Dlines$`allprotection:1_20:acopf:std`,
+              Dlines$`allprotection:1_20:scacopf:std`,
+              Dlines$`allprotection:1_20:fpacopf_09:std`,
+              Dlines$`allprotection:1_20:fpacopf_12:std`,
+              Dlines$`allprotection:1_20:fpacopf_15:std`)
+stdload <-  c(Dload$`allprotection:1_20:acopf:std`,
+              Dload$`allprotection:1_20:scacopf:std`,
+              Dload$`allprotection:1_20:fpacopf_09:std`,
+              Dload$`allprotection:1_20:fpacopf_12:std`,
+              Dload$`allprotection:1_20:fpacopf_15:std`)
+medcondlines <- c(Dlines$`allprotection:1_20:acopf:medcond`,
+              Dlines$`allprotection:1_20:scacopf:medcond`,
+              Dlines$`allprotection:1_20:fpacopf_09:medcond`,
+              Dlines$`allprotection:1_20:fpacopf_12:medcond`,
+              Dlines$`allprotection:1_20:fpacopf_15:medcond`)
+medcondload <-  c(Dload$`allprotection:1_20:acopf:medcond`,
+              Dload$`allprotection:1_20:scacopf:medcond`,
+              Dload$`allprotection:1_20:fpacopf_09:medcond`,
+              Dload$`allprotection:1_20:fpacopf_12:medcond`,
+              Dload$`allprotection:1_20:fpacopf_15:medcond`)
 
+labs <- c("N-0", "N-1",unname(TeX(c(
+                                    "$\\lambda^{\\mathrm{lim}}\\,=\\,10^{-9}$",
+                                    "$\\lambda^{\\mathrm{lim}}\\,=\\,10^{-12}$",
+                                    "$\\lambda^{\\mathrm{lim}}\\,=\\,10^{-15}$"
+                                    ))))
+pdf(file="figures/avglines.pdf", width=8.96, height=6.44)
+barplot(avglines, names.arg=labs); title("average lines failed")
+dev.off()
+pdf(file="figures/avgload.pdf", width=8.96, height=6.44)
+barplot(avgload, names.arg=labs); title("average load lost")
+dev.off()
 # s <- ggplot(data=data.frame(xx=xx,yy=yy)) + geom_line(aes(x=xx,y=yy)) + scale_y_log10(breaks = trans_breaks("log10", function(x) 10^x, n=2),
 #                                       labels = trans_format("log10", math_format(10^.x)),
 #                                       minor_breaks=log10_minor_break()) + scale_x_log10(breaks = trans_breaks("log10", function(x) 10^x, n=2),
